596997c7e50a49235ed7e06c690e605f65c4836b
sakshi-darekar <sakshidarekar12@gmail.com>
2026-03-27 12:28:38 +0530

    Team Leader created a project

---
+++ src/main/java/Project1Framework/Project1Framework/App.java
@@ -0,0 +1,13 @@
+package Project1Framework.Project1Framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
